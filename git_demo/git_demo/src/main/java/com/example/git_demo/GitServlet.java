55da7d2bff94a0fca2ef16bcb609d37f0cd457c2
matengfei <matengfei@qq.com>
2023-06-13 11:51:09 +0800

    修改了GitServlet类

--- git_demo/git_demo/src/main/java/com/example/git_demo/GitServlet.java
+++ git_demo/git_demo/src/main/java/com/example/git_demo/GitServlet.java
@@ -1,4 +1,21 @@
 package com.example.git_demo;
 
-public class GitServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 蓝色 该内容被提交到了本地仓库，这个时候又修改了该文件的内容
+public class GitServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
